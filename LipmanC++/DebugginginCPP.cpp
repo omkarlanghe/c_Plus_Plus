4faf6afc510a0e2bffd4c0096fe5f0dc0b3e97f5
Omkar Langhe <langhe.omkar96@gmail.com>
2019-04-22 02:16:04 +0530

    Add files via upload

---
+++ LipmanC++/DebugginginCPP.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#define NDEBUG
+
+using namespace std;
+
+int main()
+{
+	int size = 10, userSize = 0;
+	cout<<"Enter the size"<<endl;
+	cin>>userSize;
+
+	if(userSize < 10)
+	{
+		cerr<<"Error: "<<__FILE__<<" at line: "<<__LINE__<<endl<<" Compiled on: "<<__DATE__<<" at "<<__TIME__<<endl<<" you have enter too less size"<<endl;
+	}
+	else
+	{
+		cout<<"Welcome to the world of C++"<<endl;
+	}
+	return(0);
+}
