bde690fb0fceb12aa7b765a44cee1ccdd085cebf
Omkar Langhe <langhe.omkar96@gmail.com>
2020-04-05 20:19:44 +0530

    Hacker earth problem

---
+++ LeetCodeChallanges/monkAndPowerOfTime.cpp
@@ -0,0 +1,93 @@
+/*
+The Monk is trying to explain to its users that even a single unit of time can be extremely important and to demonstrate this particular fact he gives them a challenging task.
+
+There are N processes to be completed by you, the chosen one, since you're Monk's favorite student. All the processes have a unique number assigned to them from 1 to N.
+
+Now, you are given two things:
+
+The calling order in which all the processes are called.
+The ideal order in which all the processes should have been executed.
+Now, let us demonstrate this by an example. Let's say that there are 3 processes, the calling order of the processes is: 3 - 2 - 1. The ideal order is: 1 - 3 - 2, i.e., process number 3 will only be executed after process number 1 has been completed; process number 2 will only be executed after process number 3 has been executed.
+
+Iteration #1: Since the ideal order has process #1 to be executed firstly, the calling ordered is changed, i.e., the first element has to be pushed to the last place. Changing the position of the element takes 1 unit of time. The new calling order is: 2 - 1 - 3. Time taken in step #1: 1.
+
+Iteration #2: Since the ideal order has process #1 to be executed firstly, the calling ordered has to be changed again, i.e., the first element has to be pushed to the last place. The new calling order is: 1 - 3 - 2. Time taken in step #2: 1.
+
+Iteration #3: Since the first element of the calling order is same as the ideal order, that process will be executed. And it will be thus popped out. Time taken in step #3: 1.
+
+Iteration #4: Since the new first element of the calling order is same as the ideal order, that process will be executed. Time taken in step #4: 1.
+
+Iteration #5: Since the last element of the calling order is same as the ideal order, that process will be executed. Time taken in step #5: 1.
+
+Total time taken: 5 units.
+
+PS: Executing a process takes 1 unit of time. Changing the position takes 1 unit of time.
+
+Input format:
+The first line a number N, denoting the number of processes. The second line contains the calling order of the processes. The third line contains the ideal order of the processes.
+
+Output format:
+Print the total time taken for the entire queue of processes to be executed.
+
+Constraints:
+1<=N<=100
+
+Input: 
+3
+3 2 1
+1 3 2
+
+Output:
+5
+*/
+
+
+#include<iostream>
+using namespace std;
+
+void shiftArray(int*,int*,int);
+void rotateArrByOneEle(int*,int);
+int totalTime = 0;
+
+int main() {
+    int size;
+    int* callingOrder = new int[size];
+    int* idealOrder = new int[size];
+    
+    cin>>size;
+    
+    for(int i = 0 ; i < size ; i++) {
+        cin>>callingOrder[i];
+    }
+    for(int j = 0 ; j < size ; j++) {
+        cin>>idealOrder[j];
+    }
+    
+    shiftArray(callingOrder, idealOrder, size);
+    
+    for(int k = 0 ; k < size ; k++) {
+        if(callingOrder[k] == idealOrder[k]) {
+            callingOrder[k] = 0;
+            totalTime++;
+        }
+    }
+    cout<<totalTime<<endl;
+    delete callingOrder;
+    delete idealOrder;
+    return(0);
+}
+
+void shiftArray(int *cOrder, int *iOrder, int size) {
+    while(cOrder[0] != iOrder[0]) {
+        rotateArrByOneEle(cOrder,size);   
+    }
+}
+
+void rotateArrByOneEle(int *arr, int size) {
+    int temp = arr[0], j;
+    for(j = 0 ; j < size - 1 ; j++) {
+        arr[j] = arr[j + 1];
+    }
+    arr[j] = temp;
+    totalTime++;
+}
