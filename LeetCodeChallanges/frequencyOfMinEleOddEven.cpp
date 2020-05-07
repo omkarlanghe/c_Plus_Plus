22978d73540e2721690dfab8a01c74b258e4f214
Omkar Langhe <langhe.omkar96@gmail.com>
2020-05-07 21:58:12 +0530

    Hacker earth problem

---
+++ LeetCodeChallanges/frequencyOfMinEleOddEven.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	short int t = 10;
+	int n, count;
+	int iVec[100000];
+	cin>>t;
+
+	while(t--) {
+	    count = 1;
+		cin>>n;
+		for(auto i = 0 ; i < n ; i++) {
+		    cin>>iVec[i];
+		}
+        int min = iVec[0];
+        
+		for(auto j = 1 ;  j < n ; j++) {
+		    if(min == iVec[j]) {
+		        count++;
+		    }
+		    
+		    if(min > iVec[j]) {
+		        min = iVec[j];
+		        count = 1;
+		    }
+		}
+		
+		if((count&1) == 0) {
+		    cout<<"Unlucky"<<endl;
+		} else {
+		    cout<<"Lucky"<<endl;
+		}
+	}
+	return(0);
+}
